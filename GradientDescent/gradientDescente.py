088cd5b00c7cc2f52336596fe1339c527dfeec4e
Glioual Yassine <glioualyassine@gmail.com>
2023-12-03 14:52:47 +0100

    finish testing gradientDescent algorith

--- GradientDescent/gradientDescente.py
+++ GradientDescent/gradientDescente.py
@@ -1,13 +1,13 @@
 import numpy as np ;
 
 class LinearRegression :
-    def __int__(self,lr=0.001,n_iters=10000):
+    def __init__(self,lr=0.001,n_iters=10000):
         self.learning_rate = lr
         self.n_iters = n_iters
         self.weights = None
         self.bias = None
 
-    def fill(self,X,y):  #meth to train our model
+    def fit(self,X,y):  #meth to train our model
         n_samples,n_features = X.shape
         self.weights = np.zeros(n_features)
         self.bias = 0
@@ -16,8 +16,8 @@ class LinearRegression :
             y_pred = np.dot(X,self.weights) + self.bias
             #np.dot(a,b) product the sum prod of a end b sum(ai*bi)
 
-            dw = (1/n_samples)*np.dot(X,(y_pred-y))
-            db = (1/n_samples)*sum(y_pred-y)
+            dw = (2/n_samples)*np.dot(X.T,(y_pred-y))
+            db = (2/n_samples)*sum(y_pred-y)
 
             self.weights = self.weights - self.learning_rate*dw
             self.bias = self.bias - self.learning_rate*db
@@ -26,5 +26,3 @@ class LinearRegression :
         y_pred = np.dot(X, self.weights) + self.bias
         return y_pred
 
-
-#testing
--- GradientDescent/testing.py
+++ GradientDescent/testing.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.model_selection import train_test_split
 from  sklearn import datasets
 import  matplotlib.pyplot as plt
@@ -8,4 +9,14 @@ X_train,X_test,Y_train,Y_test = train_test_split(X,y,test_size=0.2,random_state=
 
 fig = plt.figure(figsize=(8,6))
 plt.scatter(X[:,0],y,color="b",marker="o",s=30)
-plt.show()
+#plt.show()
+
+reg = LinearRegression()
+reg.fit(X_train,Y_train)
+predictions = reg.predict(X_test)
+
+def mse(y_test , predictions):
+    return np.mean((y_test-predictions)**2)
+
+mse=mse(Y_test,predictions)
+print(mse)
